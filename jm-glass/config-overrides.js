4eaa4a0f5feeac465c416e4ca51c5c547f4d8b3b
vladk <vlad_krasovsky@yahoo.com>
2021-01-23 18:51:37 -0800

    made config-overrides pretty

--- jm-glass/config-overrides.js
+++ jm-glass/config-overrides.js
@@ -6,7 +6,7 @@ module.exports = override(
   addWebpackAlias({
     '@': path.resolve(__dirname, './src'),
     '@design': path.resolve(__dirname, './src/design'),
+    '@engine': path.resolve(__dirname, '../engine/src'),
     '@components': path.resolve(__dirname, './src/components'),
-    '@engine': path.resolve(__dirname, '../engine/src')
   })
 );
